d55807def05b0461be0ce7cd88017f1819eb745d
LIYINGZHEN <maxlivinci@gmail.com>
2020-11-03 15:01:24 +0800

    update json document

--- services/index/internal/domain/query/es_query_dao.go
+++ services/index/internal/domain/query/es_query_dao.go
@@ -16,8 +16,18 @@ func (q *EsQuery) Build() elastic.Query {
 	subQueries := make([]elastic.Query, 0)
 	subQueries = append(subQueries, elastic.NewMatchQuery("linkedSchemas", q.Schema))
 	subQueries = append(subQueries, elastic.NewRangeQuery("lastChecked").Gte(q.LastChecked))
+	subQueries = append(subQueries, elastic.NewRangeQuery("locality").Gte(q.Locality))
+	subQueries = append(subQueries, elastic.NewRangeQuery("region").Gte(q.Region))
+	subQueries = append(subQueries, elastic.NewRangeQuery("country").Gte(q.Country))
 
-	query.Must(subQueries...)
+	filters := make([]elastic.Query, 0)
+	filters = append(filters, elastic.NewGeoDistanceQuery("geolocation").Lat(q.Lat).Lon(q.Lon).Distance(q.Radius))
+
+	query.Must(subQueries...).Filter(filters...)
+
+	fmt.Println("==================================")
+	fmt.Printf("query %+v \n", query)
+	fmt.Println("==================================")
 
 	return query
 }
--- services/index/internal/domain/query/es_query_dto.go
+++ services/index/internal/domain/query/es_query_dto.go
@@ -3,12 +3,14 @@ package query
 type EsQuery struct {
 	Schema      string `form:"schema"`
 	LastChecked int64  `form:"lastChecked"`
-	Latitude    string `form:"latitude"`
-	Longitude   string `form:"longitude"`
-	Radius      string `form:"radius"`
-	Locality    string `form:"locality"`
-	Region      string `form:"region"`
-	Country     string `form:"country"`
+
+	Lat    float64 `form:"latitude"`
+	Lon    float64 `form:"longitude"`
+	Radius string  `form:"radius"`
+
+	Locality string `form:"locality"`
+	Region   string `form:"region"`
+	Country  string `form:"country"`
 }
 
 type QueryResult map[string]interface{}
