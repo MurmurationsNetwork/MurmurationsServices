c6034450baf1085790908586b4c15ea27505f288
LIYINGZHEN <maxlivinci@gmail.com>
2020-11-04 16:19:33 +0800

    chore: use lat and lon

--- services/index/internal/domain/query/es_query_dto.go
+++ services/index/internal/domain/query/es_query_dto.go
@@ -4,8 +4,8 @@ type EsQuery struct {
 	Schema      string `form:"schema"`
 	LastChecked int64  `form:"lastChecked"`
 
-	Lat    *float64 `form:"latitude"`
-	Lon    *float64 `form:"longitude"`
+	Lat    *float64 `form:"lat"`
+	Lon    *float64 `form:"lon"`
 	Radius *string  `form:"radius"`
 
 	Locality *string `form:"locality"`
