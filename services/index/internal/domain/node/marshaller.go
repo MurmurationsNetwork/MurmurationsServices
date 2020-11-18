38fa99053e2a6a3d1cdd7287979ee727cba6fd0d
LIYINGZHEN <maxlivinci@gmail.com>
2020-11-18 15:43:21 +0800

    remove failure_reasons for post_failed

--- services/index/internal/domain/node/marshaller.go
+++ services/index/internal/domain/node/marshaller.go
@@ -40,7 +40,7 @@ func (node *Node) GetNodeRespond() interface{} {
 		node.ProfileHash = nil
 		node.LastValidated = nil
 	}
-	if node.Status != constant.NodeStatus.ValidationFailed && node.Status != constant.NodeStatus.PostFailed {
+	if node.Status != constant.NodeStatus.ValidationFailed {
 		node.FailureReasons = nil
 	}
 
