7ab820cb77dd8e0ffc0f7da11ddd755e4021756c
Max Li <maxlivinci@gmail.com>
2021-01-15 16:12:36 +0800

    feat: add volume for elastic search (#85)

--- services/library/internal/app/application.go
+++ services/library/internal/app/application.go
@@ -16,9 +16,9 @@ func init() {
 }
 
 func StartApplication() {
-	router := gin.Default()
-
-	router.Use(CORS())
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.New()
+	router.Use(gin.Recovery(), CORS())
 
 	mapUrls(router)
 
