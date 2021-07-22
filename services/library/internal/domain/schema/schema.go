e14f04259fe2b8fb00c111a43404d154267458ec
Max <maxlivinci@gmail.com>
2021-07-22 17:59:28 +0800

    chore: change version to string (#157)

--- services/library/internal/domain/schema/schema.go
+++ services/library/internal/domain/schema/schema.go
@@ -4,7 +4,7 @@ type Schema struct {
 	Title       string `bson:"title,omitempty"`
 	Description string `bson:"description,omitempty"`
 	Name        string `bson:"name,omitempty"`
-	Version     int    `bson:"version,omitempty"`
+	Version     string `bson:"version,omitempty"`
 	URL         string `bson:"url,omitempty"`
 }
 
