8e7339668567f8f7c4c3babd023115cdf3511617
Vincent <vincent@fishboneapps.com>
2022-01-11 20:07:51 -0500

    optimize wait_for_timeout.sh

--- wait_for_timeout.sh
+++ wait_for_timeout.sh
@@ -6,10 +6,9 @@ declare STATUS=$2
 declare TIMEOUT=$3
 
 HOST=$HOST STATUS=$STATUS timeout --foreground -s TERM $TIMEOUT bash -c \
-    'sleep 30;\
-    while [[ ${STATUS_RECEIVED} != ${STATUS} ]];\
-        do STATUS_RECEIVED=$(curl -s -o /dev/null -L -w ''%{http_code}'' ${HOST}) && \
-        echo "received status: $STATUS_RECEIVED" && \
-        sleep 30;\
+    'while [[ ${STATUS_RECEIVED} != ${STATUS} ]];\
+        do sleep 30 && \
+        STATUS_RECEIVED=$(curl -s -o /dev/null -L -w ''%{http_code}'' ${HOST}) && \
+        echo "received status: $STATUS_RECEIVED"; \
     done;
     echo success with status: $STATUS_RECEIVED'
