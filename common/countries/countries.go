ebf55261a97081435f100c12b3cd277ce30c2196
Vincent <vincent@fishboneapps.com>
2022-01-21 22:35:15 -0500

    add log to check directory

--- common/countries/countries.go
+++ common/countries/countries.go
@@ -5,10 +5,23 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 func FindAlpha2ByName(country interface{}) (countryCode string, err error) {
+	path, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(path)
+
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(dir)
+
 	f, err := os.Open("countries.json")
 	fmt.Println(err)
 	if err != nil {
