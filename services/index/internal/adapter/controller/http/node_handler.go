208ead4ae457cd16ec371d3ac87f0828351823db
Vincent Wang <vincent@fishboneapps.com>
2022-02-11 03:36:15 -0500

    add nodes-sync (#244)

--- services/index/internal/adapter/controller/http/node_handler.go
+++ services/index/internal/adapter/controller/http/node_handler.go
@@ -1,7 +1,9 @@
 package http
 
 import (
+	"github.com/MurmurationsNetwork/MurmurationsServices/common/constant"
 	"net/http"
+	"time"
 
 	"github.com/MurmurationsNetwork/MurmurationsServices/common/resterr"
 	"github.com/MurmurationsNetwork/MurmurationsServices/services/index/internal/entity/query"
@@ -14,6 +16,7 @@ type NodeHandler interface {
 	Get(c *gin.Context)
 	Search(c *gin.Context)
 	Delete(c *gin.Context)
+	AddSync(c *gin.Context)
 }
 
 type nodeHandler struct {
@@ -103,4 +106,52 @@ func (handler *nodeHandler) Delete(c *gin.Context) {
 	}
 
 	c.Status(http.StatusOK)
+}
+
+func (handler *nodeHandler) AddSync(c *gin.Context) {
+	var node nodeDTO
+	if err := c.ShouldBindJSON(&node); err != nil {
+		restErr := resterr.NewBadRequestError("Invalid JSON body.")
+		c.JSON(restErr.Status(), restErr)
+		return
+	}
+
+	if err := node.Validate(); err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+
+	result, err := handler.nodeUsecase.AddNode(node.toEntity())
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+
+	// default time interval is 5 seconds
+	waitInterval := 5 * time.Second
+	retries := 5
+
+	for retries != 0 {
+		nodeInfo, err := handler.nodeUsecase.GetNode(result.ID)
+		if err != nil {
+			c.JSON(err.Status(), err)
+			return
+		}
+
+		if nodeInfo.Status == constant.NodeStatus.Posted {
+			c.JSON(http.StatusOK, handler.toGetNodeVO(nodeInfo))
+			return
+		}
+
+		if nodeInfo.Status == constant.NodeStatus.ValidationFailed || nodeInfo.Status == constant.NodeStatus.PostFailed {
+			c.JSON(http.StatusBadRequest, handler.toGetNodeVO(nodeInfo))
+			return
+		}
+
+		time.Sleep(waitInterval)
+		retries--
+	}
+
+	// if server can't get the node with posted or failed information, return the node id for user to get the node in the future.
+	c.JSON(http.StatusOK, handler.toAddNodeVO(result))
 }
