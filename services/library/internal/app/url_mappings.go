4ea44163d75b6a3767f2f9151860fac99caffcab
Geoff Turk <geoff@ic3.dev>
2022-02-12 10:20:45 +0100

    Revert library back to v1

--- services/library/internal/app/url_mappings.go
+++ services/library/internal/app/url_mappings.go
@@ -8,19 +8,14 @@ import (
 )
 
 func mapUrls(router *gin.Engine) {
-	deprecationHandler := http.NewDeprecationHandler()
 	pingHandler := http.NewPingHandler()
 	schemaHandler := http.NewSchemaHandler(service.NewSchemaService(db.NewSchemaRepo()))
 
 	v1 := router.Group("/v1")
 	{
-		v1.Any("/*any", deprecationHandler.DeprecationV1)
-	}
+		v1.GET("/ping", pingHandler.Ping)
+		v1.GET("/schemas", schemaHandler.Search)
 
-	v2 := router.Group("/v2")
-	{
-		v2.GET("/ping", pingHandler.Ping)
-		v2.GET("/schemas", schemaHandler.Search)
 	}
 
 }
