577753035035da4cc0386f164067dc5986dafdd5
Vincent Wang <vincent@fishboneapps.com>
2022-02-12 04:29:42 -0500

    feat: add v2 URL and v1 deprecated message (#246)

--- services/index/internal/app/url_mappings.go
+++ services/index/internal/app/url_mappings.go
@@ -8,15 +8,25 @@ import (
 )
 
 func mapUrls(router *gin.Engine) {
-	nodeHandler := http.NewNodeHandler(usecase.NewNodeService(db.NewRepository()))
-	router.POST("/nodes", nodeHandler.Add)
-	router.GET("/nodes/:nodeId", nodeHandler.Get)
-	router.GET("/nodes", nodeHandler.Search)
-	router.DELETE("/nodes/:nodeId", nodeHandler.Delete)
-
-	// synchronously response
-	router.POST("/nodes-sync", nodeHandler.AddSync)
-
+	deprecationHandler := http.NewDeprecationHandler()
 	pingHandler := http.NewPingHandler()
-	router.GET("/ping", pingHandler.Ping)
+	nodeHandler := http.NewNodeHandler(usecase.NewNodeService(db.NewRepository()))
+
+	v1 := router.Group("/v1")
+	{
+		v1.Any("/*any", deprecationHandler.DeprecationV1)
+	}
+
+	v2 := router.Group("/v2")
+	{
+		v2.GET("/ping", pingHandler.Ping)
+
+		v2.POST("/nodes", nodeHandler.Add)
+		v2.GET("/nodes/:nodeId", nodeHandler.Get)
+		v2.GET("/nodes", nodeHandler.Search)
+		v2.DELETE("/nodes/:nodeId", nodeHandler.Delete)
+
+		// synchronously response
+		v2.POST("/nodes-sync", nodeHandler.AddSync)
+	}
 }
