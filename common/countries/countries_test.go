d58477a0cdcc361051cea35d4c7eb0e8760f2f33
TingSyuanWang <vincent@fishboneapps.com>
2023-05-21 00:16:20 -0400

    fix: fix tests in countries
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- common/countries/countries_test.go
+++ common/countries/countries_test.go
@@ -1,14 +1,25 @@
 package countries
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/go-playground/assert/v2"
 )
 
 func TestFindAlpha2ByName(t *testing.T) {
+	// Create a mock HTTP server
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Return a sample response
+		response := `{"TD": ["chad"]}`
+		w.Write([]byte(response))
+	}))
+	defer mockServer.Close()
+
+	url := mockServer.URL
+
 	var country interface{} = "Chad"
-	url := "https://raw.githubusercontent.com/MurmurationsNetwork/MurmurationsLibrary/main/countries/map.json"
 	countryIso, err := FindAlpha2ByName(url, country)
 	assert.Equal(t, "TD", countryIso)
 	assert.Equal(t, nil, err)
