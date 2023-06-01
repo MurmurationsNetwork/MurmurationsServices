07fe2ee0f6b6f912c8a6e98f02d9d0130f4e728a
Geoff Turk <geoff@ic3.dev>
2023-06-01 13:57:31 +0200

    docs: explain cmd/SERVICE_NAME and their main functions

--- cmd/library/main.go
+++ cmd/library/main.go
@@ -1,3 +1,8 @@
+// This is a Go program that defines the main function for the library service.
+// The import statements import two packages: logger and library. The logger
+// package is used for logging messages, while the library package contains the
+// implementation of the library service.
+
 package main
 
 import (
@@ -6,14 +11,25 @@ import (
 )
 
 func main() {
-	logger.Info("Library service starting")
+	// Start by logging a message to indicate that the service is starting up.
+	logger.Info("Library service starting.")
 
+	// Create a new instance of the library.Service struct using the
+	// library.NewService function. This function initializes the service and
+	// returns a pointer to the Service struct.
 	s := library.NewService()
 
+	// Start a new goroutine that waits for the service to start up using the
+	// WaitUntilUp method of the Service struct. This method returns a channel
+	// that is closed when the service is up and running. The goroutine waits
+	// for the channel to be closed and then logs a message to indicate that the
+	// service has started.
 	go func() {
 		<-s.WaitUntilUp()
 		logger.Info("Library service started")
 	}()
 
+	// Call the Run method of the Service struct to start the service. This
+	// method blocks until the service is shut down.
 	s.Run()
 }
--- cmd/validation/main.go
+++ cmd/validation/main.go
@@ -1,3 +1,8 @@
+// This is a Go program that defines the main function for the validation
+// service. The import statements import two packages: logger and validation.
+// The logger package is used for logging messages, while the validation package
+// contains the implementation of the validation service.
+
 package main
 
 import (
@@ -6,14 +11,25 @@ import (
 )
 
 func main() {
+	// Start by logging a message to indicate that the service is starting up.
 	logger.Info("Validation service starting")
 
+	// Create a new instance of the validation.Service struct using the
+	// validation.NewService function. This function initializes the service and
+	// returns a pointer to the Service struct.
 	s := validation.NewService()
 
+	// Start a new goroutine that waits for the service to start up using the
+	// WaitUntilUp method of the Service struct. This method returns a channel
+	// that is closed when the service is up and running. The goroutine waits
+	// for the channel to be closed and then logs a message to indicate that the
+	// service has started.
 	go func() {
 		<-s.WaitUntilUp()
 		logger.Info("Validation service started")
 	}()
 
+	// Call the Run method of the Service struct to start the service. This
+	// method blocks until the service is shut down.
 	s.Run()
 }
