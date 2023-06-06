dca9edc702361e02d0fd2e029819ee2a786d9d2b
Geoff Turk <geoff@ic3.dev>
2023-06-06 16:56:19 +0200

    docs: modify comments

--- services/validation/internal/model/node.go
+++ services/validation/internal/model/node.go
@@ -1,9 +1,10 @@
 package model
 
-// Node represents a node in a network.
+// Node represents a node stored in the index.
 type Node struct {
 	// ProfileURL is the URL of the node's profile.
 	ProfileURL string `json:"profile_url"`
-	// Version is the version number of the node.
+	// Version is the version vector of the node.
+	// https://en.wikipedia.org/wiki/Version_vector
 	Version int32 `json:"version"`
 }
