489b5c9bc339211965210883ce5e387d98660119
Geoff Turk <geoff@ic3.dev>
2023-06-20 23:15:05 +0200

    docs: remove periods from phrases

--- services/library/config/config.go
+++ services/library/config/config.go
@@ -16,34 +16,34 @@ type Config struct {
 
 // serverConfig holds server-specific configuration.
 type serverConfig struct {
-	// Server's port.
+	// Server's port
 	Port string `env:"SERVER_PORT,required"`
-	// Timeout for reading data.
+	// Timeout for reading data
 	TimeoutRead time.Duration `env:"SERVER_TIMEOUT_READ,required"`
-	// Timeout for writing data.
+	// Timeout for writing data
 	TimeoutWrite time.Duration `env:"SERVER_TIMEOUT_WRITE,required"`
-	// Timeout for idle connections.
+	// Timeout for idle connections
 	TimeoutIdle time.Duration `env:"SERVER_TIMEOUT_IDLE,required"`
-	// Rate limit period for GET requests.
+	// Rate limit period for GET requests
 	GetRateLimitPeriod string `env:"GET_RATE_LIMIT_PERIOD,required"`
-	// Rate limit period for POST requests.
+	// Rate limit period for POST requests
 	PostRateLimitPeriod string `env:"POST_RATE_LIMIT_PERIOD,required"`
 }
 
 // mongoConfig holds MongoDB-specific configuration.
 type mongoConfig struct {
-	// MongoDB username.
+	// MongoDB username
 	Username string `env:"MONGO_USERNAME,required"`
-	// MongoDB password.
+	// MongoDB password
 	Password string `env:"MONGO_PASSWORD,required"`
-	// MongoDB host.
+	// MongoDB host
 	Host string `env:"MONGO_HOST,required"`
-	// MongoDB database name.
+	// MongoDB database name
 	DBName string `env:"MONGO_DB_NAME,required"`
 }
 
 // staticConfig holds static file-specific configuration.
 type staticConfig struct {
-	// Path to the static files.
+	// Path to the static files
 	StaticFilePath string `env:"STATIC_FILE_PATH,required"`
 }
