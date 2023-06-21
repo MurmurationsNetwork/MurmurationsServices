1f77e8a4d6715fac86331c1627b453970281edc1
Ting-Hsuan Wang <vincent@fishboneapps.com>
2023-06-21 06:45:32 -0400

    refactor: ignore redirected `profile_url`s (#518)

--- pkg/httputil/httputil.go
+++ pkg/httputil/httputil.go
@@ -118,3 +118,20 @@ func GetJSONStr(source string) (string, error) {
 	}
 	return buffer.String(), nil
 }
+
+func CheckRedirect(url string) (bool, error) {
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	resp, err := client.Get(url)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 300 && resp.StatusCode <= 399 {
+		return true, nil
+	}
+	return false, nil
+}
