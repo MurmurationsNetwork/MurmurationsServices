12ac9523b70f16e3b67f49aae25109af77c9a9df
Vincent Wang <vincent@fishboneapps.com>
2023-07-03 02:01:32 -0400

    refactor: add error message in dataproxyupdater (#532)

--- services/cronjob/dataproxyupdater/cmd/dataproxyupdater/main.go
+++ services/cronjob/dataproxyupdater/cmd/dataproxyupdater/main.go
@@ -279,7 +279,7 @@ func getURL(
 func getProfiles(url string) ([]map[string]interface{}, error) {
 	res, err := httputil.Get(url)
 	if err != nil {
-		return nil, fmt.Errorf("can't get data from" + url)
+		return nil, fmt.Errorf("can't get data from " + url + "with the error message: " + err.Error())
 	}
 
 	defer res.Body.Close()
