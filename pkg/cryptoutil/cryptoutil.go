feaf5cb0c4f307c0fec72c7f4d4e00139926ea86
maxlivinci <maxlivinci@gmail.com>
2023-09-11 18:47:32 -0700

    update comment

--- pkg/cryptoutil/cryptoutil.go
+++ pkg/cryptoutil/cryptoutil.go
@@ -6,7 +6,6 @@ import (
 )
 
 // ComputeSHA256 computes the SHA-256 hash of the given input string.
-// If the input is a JSON string, please use jsonutil.Hash.
 func ComputeSHA256(input string) string {
 	hash := sha256.Sum256([]byte(input))
 	return hex.EncodeToString(hash[:])
