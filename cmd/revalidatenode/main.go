41b547878adf975380a6c0a09d203d7401c93c60
maxlivinci <maxlivinci@gmail.com>
2023-10-02 14:19:48 -0700

    log duration & prevent concurrent cronjob

--- cmd/revalidatenode/main.go
+++ cmd/revalidatenode/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/MurmurationsNetwork/MurmurationsServices/pkg/logger"
 	"github.com/MurmurationsNetwork/MurmurationsServices/services/cronjob/revalidatenode/pkg/revalidatenode"
 )
@@ -9,10 +11,15 @@ func main() {
 	logger.Info("Starting node revalidation process...")
 
 	s := revalidatenode.NewCronJob()
+
+	startTime := time.Now()
+
 	if err := s.Run(); err != nil {
 		logger.Panic("Failed to revalidate nodes: ", err)
 		return
 	}
 
+	duration := time.Since(startTime)
 	logger.Info("Node revalidation process completed successfully")
+	logger.Info("Node revalidation run duration: " + duration.String())
 }
