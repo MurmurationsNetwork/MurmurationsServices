5ddefa254e82a2deab194a5bc4c8838794bc9b16
maxlivinci <maxlivinci@gmail.com>
2023-12-01 12:38:59 -0800

    run e2e test

--- scripts/wait_for_timeout.sh
+++ scripts/wait_for_timeout.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-# This is a bash script that checks the HTTP status code of a given host until
-# it matches a desired status code or until a specified timeout.
+# This script checks the HTTP status code of a given host until it matches a
+# desired status code or until a specified timeout. It uses exponential backoff
+# for retrying.
 
 set -eux
 
 declare HOST=$1
 declare STATUS=$2
 declare TIMEOUT=$3
+declare SLEEP_TIME=2 # initial sleep time in seconds
+declare MAX_SLEEP=120 # maximum sleep time in seconds
 
 HOST=$HOST STATUS=$STATUS timeout --foreground -s TERM $TIMEOUT bash -c \
-    'while [[ ${STATUS_RECEIVED} != ${STATUS} ]];\
-        do sleep 30 && \
+    'while [[ ${STATUS_RECEIVED} != ${STATUS} ]]; do
+        sleep $SLEEP_TIME && \
         STATUS_RECEIVED=$(curl -s -o /dev/null -L -w ''%{http_code}'' ${HOST}) && \
-        echo "received status: $STATUS_RECEIVED"; \
-    done;
-    echo success with status: $STATUS_RECEIVED'
+        echo "received status: $STATUS_RECEIVED";
+        if [[ ${STATUS_RECEIVED} != ${STATUS} ]]; then
+            SLEEP_TIME=$((SLEEP_TIME * 2 > MAX_SLEEP ? MAX_SLEEP : SLEEP_TIME * 2))
+        fi
+     done;
+     echo success with status: $STATUS_RECEIVED'
