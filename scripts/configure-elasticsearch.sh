d31c2026f8672b7eb6751a77f1dc6e74510940c8
maxlivinci <maxlivinci@gmail.com>
2024-02-18 17:37:37 -0800

    update configure-elasticsearch.sh

--- scripts/configure-elasticsearch.sh
+++ scripts/configure-elasticsearch.sh
@@ -3,20 +3,16 @@
 # This script configures Elasticsearch pods for S3 access by updating
 # the necessary access and secret keys in each pod's Elasticsearch keystore.
 
-# Ensure we read input from the terminal even when input is redirected
-if [ -t 0 ]; then
-  # Terminal input available, proceed as normal
-  echo "Reading input from the terminal..."
-else
-  # Input redirected, force read from /dev/tty (the terminal)
-  exec < /dev/tty
+# Check if the correct number of arguments are passed
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 ACCESS_KEY SECRET_KEY NAMESPACE"
+  exit 1
 fi
 
-# Prompt for S3 access credentials and namespace.
-read -p "Enter your ACCESS_KEY: " ACCESS_KEY
-read -p "Enter your SECRET_KEY: " SECRET_KEY
-read -p "Enter your NAMESPACE (default if empty): " NAMESPACE
-NAMESPACE=${NAMESPACE:-default} # Default namespace if not specified.
+# Assign command line arguments to variables
+ACCESS_KEY=$1
+SECRET_KEY=$2
+NAMESPACE=${3:-default} # Use provided namespace or default to 'default'
 
 # Define Elasticsearch pods to configure.
 PODS="index-es-cluster-0 index-es-cluster-1 index-es-cluster-2"
