f96cc593d469a327a4c366066b2bb188a7439be1
maxlivinci <maxlivinci@gmail.com>
2024-05-03 17:01:56 -0700

    update cleanup cronjob

--- build/cleanup/docker/cleanup.sh
+++ build/cleanup/docker/cleanup.sh
@@ -5,7 +5,7 @@ NAMESPACES=("default" "murm-queue")
 for NAMESPACE in "${NAMESPACES[@]}"
 do
     echo "Checking pods in namespace: $NAMESPACE"
-    kubectl get pods -n $NAMESPACE --no-headers | grep Terminating | while read pod_name; do
+    kubectl get pods -n $NAMESPACE --no-headers | grep Terminating | awk '{print $1}' | while read pod_name; do
         echo "Deleting pod $pod_name stuck in terminating in $NAMESPACE..."
         kubectl delete pod $pod_name -n $NAMESPACE --force --grace-period=0
     done
